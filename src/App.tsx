bce8809d9ff0dce361ca39ba8ba01a7e1886ded3
Manoa Aro <hei.manoa.2@gmail.com>
2022-09-25 21:04:20 +0200

    upload files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+import PokemonCard from './components/pokemon-card';
+import './App.css';
+import PokemonList from './pages/pokemon-list';
+
+const App: FunctionComponent = () => {
+  return (
+  <>
+      <PokemonList/>
+  </>
+  )
+}
+
+export default App;
+ 
---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from 'react';
+import Pokemon from '../models/pokemon';
+import './pokemon-card.css';
+type Props = {
+  pokemon: Pokemon
+};
+
+const PokemonCard: FunctionComponent<Props> = ({ pokemon }) => {
+
+  return (
+    <div className="col s6 m4">
+      <div className="card horizontal">
+        <div className="card-image">
+          <img src={pokemon.picture} alt={pokemon.name} />
+        </div>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p>{pokemon.name}</p>
+            <p><small>{pokemon.created.toString()}</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonCard;
